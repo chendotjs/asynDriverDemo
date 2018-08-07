e010ef0199bc15f4e509112602daa2f741eda520
chendotjs <chendotjs@qq.com>
2018-08-07 21:56:00 +0800

    Modify RBV value.

--- dummyApp/src/drvDummy.h
+++ dummyApp/src/drvDummy.h
@@ -2,57 +2,57 @@
 #define ASYN_DRV_DUMMY_H_
 
 #include <cstdint>
-#include <iostream>
-#include <memory>
 #include <functional>
+#include <iostream>
 #include <map>
+#include <memory>
 
-#include <iocsh.h>
+#include <asynPortDriver.h>
+#include <epicsEvent.h>
 #include <epicsExport.h>
 #include <epicsThread.h>
-#include <epicsEvent.h>
 #include <epicsTimer.h>
 #include <epicsTypes.h>
-#include <asynPortDriver.h>
+#include <iocsh.h>
 
 /* NOTE bind `epics pv` to this `parm string`
- * The `parm string` will bind to `param value` (which is mapped to device property 
+ * The `parm string` will bind to `param value` (which is mapped to device
+ * property
  */
-#define DummyEchoStr	"DUMMY_ECHO"	///< asynInt32, r/w echo trigger
+#define DummyEchoStr "DUMMY_ECHO" ///< asynInt32, r/w echo trigger
 
 using namespace std;
 
 class DummyDriver : asynPortDriver {
-	public:
-		DummyDriver(const char *portName);
-		virtual ~DummyDriver();
-		/* NOTE define `param value` */
-		struct Params {
-			int echo;
-		};
-		struct Params params;
-//		std::unique_ptr<struct Params> params = std::unique_ptr<struct Params>(new struct Params);
-		static const size_t NUM_PARAMS; // need `static` to be initialized before constructor
+public:
+  DummyDriver(const char *portName);
+  virtual ~DummyDriver();
+  /* NOTE define `param value` */
+  struct Params {
+    int echo;
+  };
+  struct Params params;
+  //		std::unique_ptr<struct Params> params = std::unique_ptr<struct
+  //Params>(new struct Params);
+  static const size_t
+      NUM_PARAMS; // need `static` to be initialized before constructor
 
-		virtual asynStatus writeInt32(asynUser *, epicsInt32) override;
+  virtual asynStatus writeInt32(asynUser *, epicsInt32) override;
 
-		template <typename T>
-		using DummyDriverFuncPtr = int (DummyDriver::*)(asynUser *, T *);
+  template <typename T>
+  using DummyDriverFuncPtr = int (DummyDriver::*)(asynUser *, T *);
 
-		void registerHandler_Int32(int, DummyDriverFuncPtr<epicsInt32>);
+  void registerHandler_Int32(int, DummyDriverFuncPtr<epicsInt32>);
 
-	protected:
-		template <typename T>
-		using handler_t = std::function<int(asynUser *, T*)>;
+protected:
+  template <typename T> using handler_t = std::function<int(asynUser *, T *)>;
 
-		template <typename T>
-		using handlerMap_t = std::map<int, handler_t<T>>;
+  template <typename T> using handlerMap_t = std::map<int, handler_t<T>>;
 
-		handlerMap_t<epicsInt32> handlerMap_Int;
+  handlerMap_t<epicsInt32> handlerMap_Int;
 
-		// handlers...
-		int echoHandler(asynUser *, epicsInt32 *);
-
+  // handlers...
+  int echoHandler(asynUser *, epicsInt32 *);
 };
 
 // XXX my implementation is different from AreaDetector
